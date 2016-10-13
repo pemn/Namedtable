4798cce9433c6897e7c667b3c745b485c683d544
Paulo Ernesto <pemn@users.noreply.github.com>
2016-10-13 09:04:04 -0300

    Add files via upload

---
+++ examples/seatbelts_statistics.pl
@@ -0,0 +1,13 @@
+#!perl
+# example for Namedtables.pm
+# using state x77 sample data
+
+
+use strict;
+use Namedtable;
+
+my $table = new Namedtable('seatbelts.csv');
+
+my @statistics = (['year','breakdown'],['law','breakdown'],['DriversKilled','sum'],['kms','sum'],['PetrolPrice','mean'],['DriversKilled(death by kms)','mean','kms']);
+
+$table->breakdownToFile('seatbelts_statistics_output.csv', @statistics);
